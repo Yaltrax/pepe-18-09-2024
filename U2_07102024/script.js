8f49a7c26fdd67984434b13f8f143de045c67575
Yael Nava <yael.nava2007@hotmail.com>
2024-10-11 00:07:58 -0600

    Actualización en el archivo index.html

--- U2_07102024/script.js
+++ U2_07102024/script.js
@@ -1,21 +1,24 @@
-function appendToDisplay(value) {
+function appendToDisplay(value) { // Este agrega el valor recibido al contenido actual del elemento con id 'display'
     document.getElementById('display').value += value;
 }
 
-function clearDisplay() {
+function clearDisplay() { // Limpia el contenido del elemento con id 'display', dejándolo vacío
     document.getElementById('display').value = '';
 }
 
-function deleteLast() {
-    let displayValue = document.getElementById('display').value;
+function deleteLast() { // Obtiene el valor actual del elemento con id 'display'
+    let displayValue = document.getElementById('display').value; // Es el que elimina el último carácter del valor actual del elemento con id 'display'
     document.getElementById('display').value = displayValue.slice(0, -1);
 }
 
 function calculateResult() {
     try {
+        // Evalúa la expresión matemática en el elemento con id 'display' y almacena el resultado
         let result = eval(document.getElementById('display').value);
+        // Muestra el resultado en el elemento con id 'display'
         document.getElementById('display').value = result;
     } catch (error) {
+        // Si ocurre un error durante la evaluación, muestra una alerta indicando que la expresión es inválida
         alert('Expresión inválida');
     }
 }
